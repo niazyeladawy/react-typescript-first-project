e8c34d1ede3a09562a4f2a3dede69a78c50edf15
= <niazyadawy2@gmail.com>
2023-12-18 20:23:17 +0200

    lesson 2

---
+++ src/components/Todo.tsx
@@ -0,0 +1,8 @@
+
+const Todo: React.FC<{ text: string }> = (props) => {
+    return (
+        <li >{props.text}</li>
+    )
+}
+
+export default Todo
---
+++ src/components/Todos.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Todo from "../models/todo"
+import TodoItem from "./Todo"
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+    return (
+        <ul>
+            {
+                props.items.map(item => <TodoItem text={item.text} key={item.id} />)
+            }
+        </ul>
+    )
+}
+
+export default Todos
